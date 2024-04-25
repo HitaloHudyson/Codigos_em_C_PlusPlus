2becf279310c8e3a5e7171074954df43c8c93070
Hitalo Hudyson <167660187+HitaloHudyson@users.noreply.github.com>
2024-04-25 19:04:13 -0300

    Update

---
+++ Definir_Precisao.cpp
@@ -0,0 +1,31 @@
+#include <iomanip>
+
+//A biblioteca iomanip fornece manipuladores de entrada e saidade de dados//
+//Como eu precisei definir quantos numeros iriam ter apos a virgula com o std::setprecision() //
+//A biblioteca <iomanip> é utilizada junto com a biblioteca <iostream> e <fstream> //
+
+using namespace std;
+
+int main (){
+    double raio, area;
+    cin >> raio;
+    area = 3.14159 * (raio * raio);
+    cout << fixed << setprecision(4) <<"A=" << area << endl;
+
+    //A precisão foi definida aqui//
+    //se for necessario imprimir outros numeros eles virão com a precisão que foi definida //
+    //então se quisermos outros numeros com precisão diferente é necessario definir novamente a precisão //
+
+    //exemplo//
+
+double exemplo = 3.141592;
+    cout << "\nEXEMPLO 1:\n" << exemplo; 
+    //imprime 3.1416// 
+
+    //Para retornar a precisão padrão//
+
+    cout << defaultfloat << "\nEXEMPLO 2:\n" << exemplo;
+    //retorna a precisão padrão//
+
+    return 0;
+}
