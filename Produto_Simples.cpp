75e23493db79d9e488f4fcf45be2e4f27e351d26
Hitalo Hudyson <167660187+HitaloHudyson@users.noreply.github.com>
2024-04-25 18:00:50 -0300

    Update

---
+++ Produto_Simples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int termo1, termo2, produto;
+
+    cin >> termo1 >> termo2;
+
+    produto = termo1 * termo2;
+
+    cout << "PROD = " <<produto << "\n";
+
+    return 0;
+}
