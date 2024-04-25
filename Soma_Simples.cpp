0592ec3f9d0e0a3096735db7d2e46ebd79b9025e
Hitalo Hudyson <167660187+HitaloHudyson@users.noreply.github.com>
2024-04-25 18:27:25 -0300

    Update

---
+++ Soma_Simples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int termo1, termo2, soma;
+
+    cin >> termo1 >> termo2;
+
+    soma = termo1 + termo2;
+
+    cout << "SOMA = " << soma << "\n";
+
+return 0;
+}
